4aefaf22b2756ba72a6e4e242cf627cf1e5db6bb
Adeenashaikh9206 <160261894+Adeenashaikh9206@users.noreply.github.com>
2024-04-30 18:00:07 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() *10 +1);
+
+
+ const answers = await inquirer.prompt([
+   
+    {
+        type: "number",
+        name: "userNumberguessing",
+        message: "guess the number between 1-10"
+    },
+   
+])
+console.log(randomNumber);
+
+if (answers.userNumberguessing === randomNumber) {
+    console.log("you win");
+}
+else{
+    console.log("you lose");
+}
